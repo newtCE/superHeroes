1354473eb469d79d05fc7a8e577664a6eac25912
Newt Industries <info@newtindustries.com>
2020-01-03 16:18:54 -0600

    created and tested methods in the heroContoller for index and creation of a new hero

---
+++ SuperHeroes/Controllers/HeroController.cs
@@ -0,0 +1,94 @@
+﻿using SuperHeroes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperHeroes.Controllers
+{
+    public class HeroController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        // GET: Hero
+        public ActionResult Index()
+        {
+            List<SuperHero> heroList = context.SuperHeroes.ToList();
+            return View(heroList);
+        }
+
+        // GET: Hero/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Hero/Create
+        public ActionResult Create()
+        {
+            SuperHero superHero = new SuperHero();
+            return View(superHero);
+        }
+
+        // POST: Hero/Create
+        [HttpPost]
+        public ActionResult Create(SuperHero superHero)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                context.SuperHeroes.Add(superHero);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Hero/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Hero/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Hero/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Hero/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
